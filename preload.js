6840d9f793d76d231a526c425daac40204891abd
dubox <dubox@duboxdeMacBook-Pro.local>
2020-05-03 17:46:43 +0800

    dev

--- preload.js
+++ preload.js
@@ -1,5 +1,9 @@
 const http = require('http');
 var os = require('os');
+
+
+var app = window.app = {};
+
 //获取内网ip
 function getLocalIp(){
     var map = [];  
@@ -12,6 +16,44 @@ function getLocalIp(){
     }
     return '';
 }
+var f = utools.getFeatures();
+for(let i in f){
+    if(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(f[i].code)){
+        utools.removeFeature(f[i].code);
+    }
+}
+
+function addFeature(ip,id){
+    utools.setFeature({
+        "code": ""+ip,
+        "explain": "发送到："+ip,
+        // "icon": "res/xxx.png",
+        // "icon": "data:image/png;base64,xxx...",
+        // "platform": ["win32", "darwin", "linux"]
+        "cmds": [
+            {
+                "type": "regex",
+                "label": "文本正则匹配",
+                // 正则表达式字符串
+                "match":"/.+/",
+                // 长度限制（主输入框中的字符不少于） (可选)
+                //"minLength": 1,
+                // 长度限制（不多于） (可选)
+                //"maxLength": 1
+            },
+            {
+                "type": "over",
+                "label": "无匹配时",
+                // 排除的正则 (可选)
+                //"exclude":"/xxx/i",
+                // 长度限制（主输入框中的字符不少于） (可选)
+                "minLength": 1,
+                // 长度限制（不多于） (可选)
+                //"maxLength": 1
+            }
+        ]
+      })
+}
 
 window.detectDevice = function(){
 
@@ -21,9 +63,14 @@ window.detectDevice = function(){
     var ips = [];
     for(let i=1;i<256;i++){
         var ip = ipSeg.join('.') + '.' +i;
-        http.get(`http://${ip}:8891`, (res) => {
+        (function(ip){
+            http.get(`http://${ip}:8891`, (res) => {console.log(ip);console.log(res);
             ips.push(ip);
+            addFeature(ip);
+            res.resume();
           }).on('error', (err) => {});
+        })(ip);
+        
     }
     return ips;
 };
@@ -32,18 +79,46 @@ window.detectDevice = function(){
 window.http = function(){
     
 }
+
+var sendText = function(ip,text){
+    let url = `http://${ip}:8891/text?${text}`;
+    console.log(url);
+    http.get(url, (res) => {
+        res.resume();
+      }).on('error', (err) => {
+    });
+};
+
+
 utools.showNotification(`本机ip：${getLocalIp()}`, clickFeatureCode = '123', silent = true);
    
 
 
 http.get('http://127.0.0.1:8891', (res) => {
-      // ...
+    res.resume();
     }).on('error', (err) => {
       // Check if retry is needed
-      console.log(err);
+      //console.log(err);
       http.createServer((req, res) => {
+          if(req.url=="/favicon.ico"){
+            res.end();
+            return;
+          }
+          console.log(req);
+          let text = req.url.split('?');
+          text =  decodeURI(text[1]);
         res.write('hello\n');
-        //utools.showNotification('body', clickFeatureCode = '123', silent = true);
+        utools.copyText(text)
+        utools.showNotification(`"${text}"已复制到剪贴板`);
         res.end();
     }).listen(8891); //ipv6 ,'::'
 });
+// var d = 1;
+// setInterval(() => {
+//     console.log(d++);
+// }, 2000);
+
+utools.onPluginEnter(({code, type, payload, optional}) => {
+    console.log('用户进入插件', code, type, payload);
+    sendText(code,payload);
+})
